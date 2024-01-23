87d29c0d04d95d6e0b8c15e767d40515cce9eae9
kaleboni <83687079+kaleboni@users.noreply.github.com>
2024-01-23 11:43:58 -0600

    Update

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-alert('Hola Mundo');
+/* Muestra una alerta con el mensaje "¡Bienvenida y bievenido a nuestro sitio web!".*/
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
 
+/*Declara una variable llamada nombre y asígnale el valor "Luna".*/
+let nombre = 'Luna';
+console.log(nombre);
 
+/*Crea una variable llamada edady asígnale el valor 25.*/
+let edad = 25;
+console.log(edad);
+
+/*Establece una variable numeroDeVentas y asígnale el valor 50.*/
+let nunmeroDeVentas = 50;
+console.log(nunmeroDeVentas);
+
+/*Establece una variable saldoDisponible y asígnale el valor 1000.*/
+let saldoDisponible = 1000;
+console.log(saldoDisponible);
+
+/*Muestra una alerta con el texto "¡Error! Completa todos los campos".*/
+alert("¡Error! Completa todos los campos");
+
+/*Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .*/
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+/*Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .*/
+let nombreDeUsuario = prompt('Digite nombre de usuario: ');
+console.log(nombreDeUsuario);
+
+/*Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.*/
+let edadUsuario = prompt('Digite edad de usuario: ');
+console.log(edadUsuario);
+
+/*Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!". */
+if(edadUsuario >= 18){
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
